5614c6638a8037564263fd05ea741d194876768f
MAKS11060 <31521952+MAKS11060@users.noreply.github.com>
2026-05-24 15:14:39 +0200

    init

---
+++ src/workerd.ts
@@ -0,0 +1,23 @@
+export default {
+  async fetch(req, env, ctx) {
+    const _uri = new URL(req.url)
+    if (!_uri.pathname.startsWith('/https://')) return Response.json('err')
+    const uri = new URL(_uri.pathname.slice(1) + _uri.search)
+
+    return await fetch(uri, req)
+
+    // const headers = new Headers()
+    // for (const [k, v] of req.headers) {
+    //   if (k.startsWith('x-')) headers.set(k.slice(2), v)
+    // }
+
+    // const res = await fetch(uri, {
+    //   method: req.method,
+    //   headers,
+    //   body: req.body,
+    //   signal: req.signal
+    // })
+
+    // return res
+  },
+} satisfies ExportedHandler<Env>
